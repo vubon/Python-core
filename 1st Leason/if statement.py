45d6fadbc1cd24236bd9f99db94cd19682963ea2
vubon <vubon.roy@gmail.com>
2016-09-07 02:33:38 +0600

    if statement & for loop

---
+++ 1st Leason/if statement.py
@@ -0,0 +1,10 @@
+num = int(input("Please enter an integer: "))
+if num < 0:
+    num = 0
+    print('Nagative changed to zero')
+elif num == 0:
+    print('Zero')
+elif num == 1:
+    print ('Single')
+else:
+    print('more')
