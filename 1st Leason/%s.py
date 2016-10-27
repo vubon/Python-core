fd91341fe8f856ec8f6888ac688e8869e4e927be
vubon <vubon.roy@gmail.com>
2016-10-28 02:32:00 +0600

    added more basic stuffs

---
+++ 1st Leason/%s.py
@@ -0,0 +1,14 @@
+# S means converting object to string . Checkout an example
+""" def divide (a, b):
+    print("WE DIVIDING %r and %r NOW" % (a, b))
+    return a / float(b)
+print("Input first number:")
+first = float(input(">  "))
+print("OK, now input second number:")
+second = float(input(">  "))
+
+ans = divide(first, second)
+print("DONE: %r DIVIDED BY %r EQUALS %r, SWEET MATH BRO!" % (first, second, ans)) """
+
+number = 22
+print("number %d" % (number))
--- 1st Leason/if__name__.py
+++ 1st Leason/if__name__.py
@@ -1,3 +1,2 @@
 if __name__ == '__main__':
     print('I do not know about this')
-    
